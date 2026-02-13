5312635802b00c600cb12adc848e3af06bfad996
JRambousek <rambousek122@trebesin.cz>
2026-02-13 13:27:42 +0100

    Test hotovo

--- flask/app.py
+++ flask/app.py
@@ -1,17 +1,36 @@
-# Z následujících si vyber kód a sestav funkční flask aplikaci (není třeba použít vše, vyber si pouze ty části kódu, které potřebuješ)
-# Kód je funkční, pouze místo dotazníků je potřeba doplnit podle potřeby
+from flask import Flask, render_template, request, redirect, url_for
+import json
+
+app = Flask(__name__)
+
+# with open("data/recenze.json", "r") as f:
+#     recenze_json = json.load(f)
 
 
-# app = Flask(__name__)
-# from flask import Flask, render_template, request, redirect, url_for
-# @app.route("???")
-# request.args.get("???")
-# request.form.get("???")
-# print("???")
-# cursor.execute("???")
-# if request.method == "POST"
-# render_template("???", ???, ???)
+@app.route("/")
+def vitej():
+    return render_template("vitej.html")
 
+@app.route("/form")
+def form():
+    name = request.args.get("name", default="______")
+    recenze = request.args.get("recenze", default="______")
+    print(name, recenze)
+    if name and recenze:
+        if recenze == "nic":
+            return render_template("skoro.html")
+        elif len(recenze) <= 3:
+            return render_template("skoro.html")
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+    # napsana_recenze = {
+    #     "jmeno": name,
+    #     "recenze": recenze
+    # }
+    # recenze_json.append(napsana_recenze)
+
+    # with open("data/renceze.json", "w") as f:
+    #     json.dump(recenze_json, f)
+    return render_template("form.html", name=name, renceze=recenze)
+
+if __name__ == "__main__":
+    app.run(debug=True)
